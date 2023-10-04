a4792fe2341114ac9c7ea43a1a9a0936779e4bdb
Mert Demir <mert.d728@hotmail.com>
2023-10-04 17:44:05 +0200

    Add files via upload

---
+++ wa/tailwind.config.cjs
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50:  colors.indigo[50],
+          100: colors.indigo[100],
+          200: colors.indigo[200],
+          300: colors.indigo[300],
+          400: colors.indigo[400],
+          500: colors.indigo[500],
+          600: colors.indigo[600],
+          700: colors.indigo[700],
+          800: colors.indigo[800],
+          900: colors.indigo[900],
+        }
+      }
+    },
+  },
+}
